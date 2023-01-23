61b7dbca06c40830cabededa247a1cdd96507696
Trocolao <daw13.2023@gmail.com>
2023-01-23 12:31:54 +0100

    Funcion Multiplicar

--- src/principal.java
+++ src/principal.java
@@ -4,6 +4,8 @@ public class principal {
 		calculadora miCalc=new calculadora();
 		System.out.println(miCalc.sumar(5,3));
 		System.out.println(miCalc.restar(5,3));
+		System.out.println(miCalc.multiplicar(5,3));
+		
 	}
 
 }
